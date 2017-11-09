16a11adc231894117e599fb5b09ae27146d04bca
Gustavo Bueno <gustavo.bueno@keradgames.com>
2017-11-09 13:11:29 +0100

    add braintree config client token

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors()); // avoid Access-Control-Allow-Origin problem
 
+/* PAYPAL CONFIG */
 const gateway = braintree.connect({
   accessToken: process.env.ACCESS_TOKEN_SANDBOX // for production or sandbox
 });
@@ -53,9 +54,51 @@ function checkout(req: any, res: any):void {
   });
 }
 
-/* routes */
+/* BRAINTREE CONFIG */
+const gatewayBraintree = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANT_ID,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY
+});
+
+function btClientToken(req: any, res: any):void {
+  gatewayBraintree.clientToken.generate({}, (err: any, response: any) => {
+    const json = { token: response.clientToken };
+    res.send(json);
+  });
+}
+
+function btCheckout(req: any, res: any):void {
+  const paymentMethodNonce = req.body.nonce;
+  const amount = parseFloat(req.body.amount);
+
+  // Use payment method nonce here
+  const saleRequest = {
+    amount,
+    paymentMethodNonce,
+    options: {
+      submitForSettlement: true
+    }
+  };
+
+  gatewayBraintree.transaction.sale(saleRequest, (err: any, result: any) => {
+    if (err) {
+      res.send({ error: err, saleRequest });
+    } else if (result.success) {
+      res.send({ success: true, message: `Success! Transaction ID: ${result.transaction.id}`, result });
+    } else {
+      res.send({ error: result.message, saleRequest, result });
+    }
+  });
+}
+
+/* routes for paypal config */
 app.get('/client_token', clientToken);
 app.post('/checkout', checkout);
 
+/* routes for braintree config */
+app.get('/bt_client_token', btClientToken);
+app.post('/bt_checkout', btCheckout);
 
 app.listen(8001, () => console.log('Braintree server running on port 8001!'));
